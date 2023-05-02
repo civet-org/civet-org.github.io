fa3c648db1681156ff306d477f84c01c0a50fa96
Aaron <2manyvcos@squaresine.com>
2023-05-02 13:51:45 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://civet.js.org/",
+  url: "https://civet.js.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "civet-org", // Usually your GitHub org/user name.
   projectName: "civet-org.github.io", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
