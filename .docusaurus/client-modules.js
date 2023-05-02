4a7097d77b7631b6a46118b1c333d1819d75736c
Aaron <2manyvcos@squaresine.com>
2023-05-02 13:43:48 +0200

    fix

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/aaron/Projekte/github/civet/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/aaron/Projekte/github/civet/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/aaron/Projekte/github/civet/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/aaron/Projekte/github/civet/docs/src/css/custom.css'),
-];
