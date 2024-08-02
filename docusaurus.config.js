2c575b6e125949f79c643818590549f45525d729
Aaron <4709780+2manyvcos@users.noreply.github.com>
2024-08-02 15:33:02 +0200

    + update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,10 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-import lightCodeTheme from "prism-react-renderer/themes/github";
-import darkCodeTheme from "prism-react-renderer/themes/dracula";
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -20,14 +22,14 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "civet-org", // Usually your GitHub org/user name.
   projectName: "civet-org.github.io", // Usually your repo name.
-  trailingSlash: false,
 
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -39,7 +41,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/civet-org/civet-org.github.io/",
@@ -51,7 +53,7 @@ const config = {
           editUrl: "https://github.com/civet-org/civet-org.github.io/",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -130,8 +132,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Aaron Burmeister. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
